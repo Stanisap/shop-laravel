819adae681e1dd399f72511d4b187f2c89187d25
Stanislav Polushin <stanisap85@gmail.com>
2020-10-16 14:29:31 +0300

    lesson 32. Expanded product offerings - start

---
+++ app/Models/Sku.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sku extends Model
+{
+    protected $fillable = ['product_id', 'count', 'price'];
+
+//    Relations
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    //    Relations
+    // TODO: check table name and fields
+    public function skus()
+    {
+        return $this->belongsToMany(PropertyOption::class);
+    }
+}
